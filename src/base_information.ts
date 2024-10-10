7e6126387d6fa5ce92494761497eae00beb1cadc
Matthew <mdo79@uclive.ac.nz>
2024-10-10 14:06:15 +1300

    Changes for android

--- src/base_information.ts
+++ src/base_information.ts
@@ -1,4 +1,4 @@
-export const url = 'https://seng402-api.vercel.app/'
+export const url = 'http://127.0.0.1:5000' + '/' // Change this to api url
 export const user_code = 0
 
 //   https://seng402-api.vercel.app/
